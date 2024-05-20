fc2b23a2cf15aac359a55cf0b123710fac0f19c7
David Marshall <dlmarshall3@protonmail.com>
2024-05-20 15:42:39 -0500

    more changes

--- src/pages/pizza.jsx
+++ src/pages/pizza.jsx
@@ -1,6 +1,17 @@
 import { useSportsContext } from "../context/sports.context";
+import { Link } from "react-router-dom";
 
 export default function Pizza() {
   const { user } = useSportsContext();
-  return <h2>{user} likes pizza</h2>;
+  return (
+    <>
+      <h2>{user} likes pizza</h2>
+      <Link to="/">
+        <button>Go back</button>
+      </Link>
+      <Link to="/profile">
+        <button>Check out profile page</button>
+      </Link>
+    </>
+  );
 }
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,6 +1,18 @@
 import { useSportsContext } from "../context/sports.context";
 
+import { Link } from "react-router-dom";
+
 export default function Profile() {
   const { user } = useSportsContext();
-  return <h2>{user}</h2>;
+  return (
+    <>
+      <h2>Username: {user}</h2>
+      <Link to="/">
+        <button>Go back</button>
+      </Link>
+      <Link to="/pizza">
+        <button>Check out pizza page</button>
+      </Link>
+    </>
+  );
 }
